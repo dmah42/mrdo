bfab1732a6a1453988a890b21bc380b599c977f4
Dominic Hamon <dma+github@stripysock.com>
2014-09-22 22:40:04 -0700

    Add support for testing output against expectations

--- run_tests.sh
+++ run_tests.sh
@@ -2,9 +2,30 @@
 
 for t in test/*.do; do
 	echo "Running $t"
-	./mrdo --optimize=true --dump_module=false $t
-	
+
+  OUTPUT=`./mrdo --optimize=true --dump_module=false $t 2> /dev/null`
+
 	if [ $? -ne 0 ]; then
+    echo "non-zero error code"
 		break
-	fi
+  fi
+
+  if [ "$OUTPUT" != "" ]; then
+    echo "read '$OUTPUT'"
+
+    GOLD_FILE=${t%.*}.out
+    DIFF=""
+    if [ -e $GOLD_FILE ]; then
+      echo "checking '$OUTPUT' against '`cat $GOLD_FILE`' (from $GOLD_FILE)"
+      DIFF=`diff <(echo "$OUTPUT") $GOLD_FILE`
+      echo '((' $DIFF '))'
+      if [ "$DIFF" != "" ]; then
+        echo "expected `cat ${t%.*}.out`, got $OUTPUT"
+        break
+      fi
+    else
+      echo "missing expected gold file $GOLD_FILE"
+      break
+    fi
+  fi
 done
